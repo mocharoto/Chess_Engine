dd93e073a7dd496d76f23d4254fd48ddb537f60a
Seong M Kim <smkim6@asu.edu>
2017-07-21 16:13:57 -0700

    Update board.h

--- src/board.h
+++ src/board.h
@@ -5,10 +5,19 @@
 #include <iostream>
 #include "pieces.h"
 
-class Board
-{
-private:
-	
-public:
+/*
+	structure of a square
+	isOccupied will check if a piece is placed on the square
+	canMove will determine if a piece can be moved here
+	tag is the name of the given square
+*/
+struct Square {
+	bool isOccupied;
+	bool canMove;
+	std::string tag;
+};
 
-}
+struct Position {
+	int x = 0;
+	int y = 0;
+};

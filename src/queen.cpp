2035ffdd8a7810a5e146db7569dc56ea3f9bd110
Julian Yi <julian.yi26@gmail.com>
2017-07-22 20:58:06 -0700

    Add files via upload

---
+++ src/queen.cpp
@@ -0,0 +1,8 @@
+//Author: Julian Yi
+//Date Created: 22 July 2017
+//File Description: .cpp file for the Queen
+#include <iostream>
+#include <string>
+#include "pieces.h"
+#include "queen.h"
+
---
+++ src/queen.h
@@ -0,0 +1,13 @@
+//Author: Julian Yi
+//Date Created: 22 July 2017
+//File Description: Header file for the Queen
+//Fun Fact: Simon's queen is Kylee Lattomus
+#include <iostream>
+#include <string>
+#include "pieces.h"
+
+class Queen
+{
+	private:
+	public:
+} Queen;

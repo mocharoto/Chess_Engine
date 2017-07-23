ab5fa39ba7caeeaa74cbd50173ffb0b00185a371
Julian Yi <julian.yi26@gmail.com>
2017-07-22 19:40:10 -0700

    Add files via upload

---
+++ rook.cpp
@@ -0,0 +1,9 @@
+//Author: Julian Yi
+//Date Created: 22 July 2017
+//File Description: .cpp file for the Rook
+#include <iostream>
+#include <string>
+#include "pieces.h"
+#include "rook.h"
+
+
---
+++ rook.h
@@ -0,0 +1,9 @@
+//Author: Julian Yi
+//Date Created: 22 July 2017
+//File Description:Header file for Rook
+
+class Rook 
+{
+	private:
+	public:
+} Rook;

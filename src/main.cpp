bf61d39973ec674ae40321de753a8ada193716eb
dqnx <sbrock065@gmail.com>
2017-07-29 06:29:58 -0500

    Added intro to the terminal gui and key press recognition.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 //Purpose: This is a project to create a working chess engine and gui using C++.
 //File: This is the main file.
 #include <iostream>
-#include <memory>
 #include "board.h"
 #include "pawn.h"
 #include "knight.h"
@@ -11,17 +10,15 @@
 #include "coord.h"
 #include "BearLibTerminal.h"
 
-#define sizeY 8
-#define sizeX 8
-
 int main()
 {
 	board Chessboard;
-	board::Square **tile = new board::Square*[sizeY];
+	// Do these need to be on the heap?
+	board::Square **tile = new board::Square*[board::sizeY];
 
-	for (int index = 0; index < sizeY; ++index)
+	for (int index = 0; index < board::sizeY; ++index)
 	{
-		tile[index] = new board::Square[sizeX];
+		tile[index] = new board::Square[board::sizeX];
 	}
 
 	Chessboard.initializeBoard(tile);
@@ -57,26 +54,64 @@ int main()
 	//Chessboard.placePiece("Pawn1", testPawn->getPosition().x, testPawn->getPosition().y, tile);
 	//Chessboard.placePiece("Knight1", testPawn->getPosition().x, testPawn->getPosition().y, tile);
 
+
+	// Open the terminal. Since no terminal_set() method is called,
+	// the terminal will use default settings.
+	terminal_open();
+
+	// Print intro text.
+	terminal_print(1, 1, "Chess Engine");
+	terminal_print(4, 2, "by Julian Yi, Sean Brock, and Simon Kim");
+	terminal_print(1, 4, "Press Enter to start...");
+	terminal_refresh();
+
+	bool running = true;
+	while (running) {
+		// Check for input. termnial_read() is blocking, meaning the
+		// program will wait until it reads a key press.
+		auto key = terminal_read();
+
+		// Reset the terminal to blank state.
+		terminal_clear();
+
+		// Print instructions.
+		terminal_print(1, 1, "Press Enter to start...");
+
+		// Handle key presses.
+		switch (key) {
+			case TK_CLOSE:
+				running = false;
+				break;
+			case TK_ESCAPE:
+				running = false;
+				break;
+			case TK_ENTER:
+				terminal_print(1, 2, "Simon is king; Kylee is queen.");
+				break;
+			default:
+				terminal_print(1, 2, "The key pressed has no function.");
+				break;
+		}
+
+		// Commit the buffer and draw it.
+		terminal_refresh();
+
+	}
+
+	// We're done here.
+	terminal_close();
+
+	// Do we need to delete pointers if we are exiting?
+	/*
 	std::cout << "pause here" << std::endl;
 
 
-	for (int index = 0; index < sizeY; ++index)
+	for (int index = 0; index < board::sizeY; ++index)
 	{
 		delete[] tile[index];
 	}
 
 	delete[] tile;
-
-	terminal_open();
-
-	// Printing text
-    terminal_print(1, 1, "Hello, world!");
-    terminal_refresh();
-  
-    // Wait until user close the window
-    while (terminal_read() != TK_CLOSE);
-  
-    terminal_close();
-
+	*/
 	return 0;
 }

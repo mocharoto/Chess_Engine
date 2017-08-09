fdf097e91a6ac0b3823ad0b3d126688c5832af8c
dqnx <sbrock065@gmail.com>
2017-08-09 14:32:43 -0500

    Updated colors and added tile/piece color settings.

--- src/defs.h
+++ src/defs.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <algorithm>
 #include <string>
+#include <iostream>
 
 
 enum class PieceType
@@ -123,7 +124,7 @@ inline std::string colorToString(TeamColor color)
 	if (color == TeamColor::White)
 		result = "white";
 	else if (color == TeamColor::Black)
-		result = "grey";
+		result = "black";
 	else {
 		std::cout << "Error: colorToString returned None color." << std::endl;
 		result = "None";
@@ -131,22 +132,19 @@ inline std::string colorToString(TeamColor color)
 	return result;
 }
 
-/*
-enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
-enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
-enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
-enum { WHITE, BLACK, BOTH };
-enum {
-	A1 = 21, B1, C1, D1, E1, F1, G1, H1,
-	A2 = 31, B2, C2, D2, E2, F2, G2, H2,
-	A3 = 41, B3, C3, D3, E3, F3, G3, H3,
-	A4 = 51, B4, C4, D4, E4, F4, G4, H4,
-	A5 = 61, B5, C5, D5, E5, F5, G5, H5,
-	A6 = 71, B6, C6, D6, E6, F6, G6, H6,
-	A7 = 81, B7, C7, D7, E7, F7, G7, H7,
-	A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ
-};
+inline std::string colorToDraw(TeamColor color)
+{
+	std::string result;
 
-enum { FALSE, TRUE };
-*/
+	if (color == TeamColor::White)
+		result = "whitepiece";
+	else if (color == TeamColor::Black)
+		result = "blackpiece";
+	else {
+		std::cout << "Error: colorToDraw returned None color." << std::endl;
+		result = "red";
+	}
+	return result;
+}
+
 #endif

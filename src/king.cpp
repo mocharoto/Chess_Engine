1198d8fa90ee9881abc76271f1d6f3f82a46da14
mocharoto <julian.yi26@gmail.com>
2017-08-15 18:58:12 -0700

    updated king move calculation. still need to check if the square is being attacked/going to put the king in check.

--- src/king.cpp
+++ src/king.cpp
@@ -16,23 +16,36 @@ King::King(coord pos, TeamColor color) : Pieces()
 coordList King::calculateMoves(coord boundary, const squareGrid& square) const
 {
 	coordList validMoves;
-
-	// Create a vector of coords representing the moves of a king.
-	// The king can move diagonally and orthogonally.
-	coordList moves = {
-		// Diagonal unit vectors.
-		{ 1, 1 },{ 1, -1 },{ -1, 1 },{ -1, -1 },
-		// Orthogonal unit vectors.
-		{ 0, 1 },{ 0, -1 },{ 1, 0 },{ -1, 0 },
+	
+	coordList moves = 
+	{
+		//Diagonal unit vectors
+		{ 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1},
+		//Orthogonal unit vectors
+		{ 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }
 	};
 
-	for (auto& move : moves)
+	for ( auto& move : moves )
 	{
 		coord newPos = position + move;
+		//Loop each direction to adjacent sqaure.
+		if((newPos.x < boundary.y && newPos.y < boundary.y) && (newPos.x >= 0 && newPos.y >=0))
+		{
+			if(!square[newPos.x][newPos.y].occupied())
+			{
+				validMoves.push_back(newPos);
+				newPos = newPos + move;
+			}
+			else
+			{
+				if(square[newPos.x][newPos.y].pieces.front()->getTeamColor() != team)
+				{
+					validMoves.push_back(newPos);
+				}
+			}
+		}
 
-		validMoves.push_back(newPos);
-	}
-
+	}	
 
 
 
